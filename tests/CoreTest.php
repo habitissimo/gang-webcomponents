fdb87a45765bdbbd4dfde7ad6526d1880544c00f
Javier Viñas <jvinas@habitissimo.com>
2019-05-20 12:48:25 +0200

    eliminated unnecessary tests, CoreTests pass

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Gang\WebComponentsTests;
 
 use Doctrine\Common\Cache\FilesystemCache;
+use Gang\WebComponents\Configuration;
 use Gang\WebComponents\Parser\NewParser;
 use Gang\WebComponents\WebComponentController;
 use Gang\WebComponents\HTMLComponent;
@@ -36,8 +37,8 @@ final class CoreTests extends TestCase
 <html lang="es-ES">
 <head>
 
-  <link rel="stylesheet" type="text/css" href="https://www.habitissimo.es/static/build/css/frontend.min.css?v=1550482066"/>
-  <link rel="stylesheet" type="text/css" href="https://www.habitissimo.es/static/font/iconissimo_e24da5260b1055faad15d85cb5fadd90.woff"/>
+  <link rel="stylesheet" type="text/css" href="https://www.habitissimo.es/static/build/css/frontend.min.css?v=1550482066">
+  <link rel="stylesheet" type="text/css" href="https://www.habitissimo.es/static/font/iconissimo_e24da5260b1055faad15d85cb5fadd90.woff">
   <noscript><style type="text/css">img.lazy{display:none;}</style></noscript>
   <script type="text/javascript" src="https://www.habitissimo.es/static/build/js/habitissimo-frontend.min.js?v=1550482066"></script>
   <script type="text/javascript" src="//static.criteo.net/js/ld/ld.js" async></script><script type="text/javascript">window.criteo_q = window.criteo_q || [];window.criteo_q.push({ event: "setAccount", account: 0 },{ event: "setSiteType", type:"d"},{ event: "viewHome" });</script>
@@ -52,23 +53,25 @@ final class CoreTests extends TestCase
     public function setup()
     {
         $this->prophet = new Prophet;
-        $this->parser = new NewParser();
         $this->renderer = $this->prophet->prophesize(TreeRenderer::class);
         $this->loader = $this->prophet->prophesize(ComponentLibrary::class);
         $this->factory = $this->prophet->prophesize(HTMLComponentFactory::class);
 
 
-        $lib = new ComponentLibrary( null);
-        $lib->loadLibrary("Gang\WebComponentsTests\WebComponents", __DIR__ . DIRECTORY_SEPARATOR .  "WebComponents");
+        Configuration::$library_base_namespace = "Gang\WebComponentsTests\WebComponents";
+        Configuration::$library_template_dir = __DIR__ . DIRECTORY_SEPARATOR .  "WebComponents";
+        $lib = new ComponentLibrary();
+
 
         $this->controller = new WebComponentController(
-          $lib, $this->renderer = new TreeRenderer($lib), $this->parser
+          $lib
         );
     }
 
     public function testPlainHTMLOnly() : void
     {
-        $an_htrml_string = '<a href="foo">Goto my site</a>';
+        $a = '<a href="foo">Goto my site</a>';
+        $an_htrml_string = "<!DOCTYPE html>". "\n" . $a . "\n";
         $result = $this->controller->process($an_htrml_string);
 
         $this->assertEquals($an_htrml_string, $result);
@@ -76,8 +79,9 @@ final class CoreTests extends TestCase
 
     public function testWebComponentOnly()
     {
-        $an_htrml_string = "<Button href='foo'>Goto my site</Button>";
-        $expected_render = '<a role="button" href="foo">Goto my site</a>';
+      $a = '<a role="button" href="foo">Goto my site</a>';
+        $an_htrml_string = "<!DOCTYPE html><wc-button href='foo'>Goto my site</wc-button>";
+        $expected_render = '<!DOCTYPE html>' . "\n" . $a . "\n";
 
         $result = $this->controller->process($an_htrml_string);
         $this->assertEquals($expected_render, $result);
@@ -85,11 +89,11 @@ final class CoreTests extends TestCase
 
     public function testHTMLAndWebComponent() : void
     {
-        $non_wc = '<img src="foo"/>';
-        $wc = '<Button href="foo">Go to my site</Button>';
+        $non_wc = '<img src="foo">';
+        $wc = '<wc-button href="foo">Go to my site</wc-button>';
         $rendered_wc = '<a role="button" href="foo">Go to my site</a>';
-        $input = $non_wc . $wc;
-        $expected_render = $non_wc . $rendered_wc;
+        $input = "<!DOCTYPE html>" . $non_wc . $wc;
+        $expected_render ="<!DOCTYPE html>". "\n". $non_wc . $rendered_wc . "\n";
 
         $result = $this->controller->process($input);
 
@@ -101,35 +105,29 @@ final class CoreTests extends TestCase
      */
     public function should_integrate_web_components() : void
     {
-        $lib = new ComponentLibrary(null);
-        $lib->loadLibrary("Gang\WebComponentsTests\WebComponents", __DIR__ . DIRECTORY_SEPARATOR .  "WebComponents");
+        Configuration::$library_template_dir = __DIR__ . DIRECTORY_SEPARATOR .  "WebComponents";
+        Configuration::$library_base_namespace = "Gang\WebComponentsTests\WebComponents";
+        $lib = new ComponentLibrary();
         $controller = new WebComponentController($lib);
         $parsed_template = $controller->process(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR .  "WebComponents" . DIRECTORY_SEPARATOR ."IntegrationTest.twig"));
         $this->assertEquals($this->integrationHtmlResult,$parsed_template);
     }
 
-//     public function testComplexRadioInputGroup() : void
-//     {
-//         $lib = new ComponentLibrary();
-//         $lib->loadLibrary("Habitissimo\Utils\Web\Src\Component", __DIR__ . DIRECTORY_SEPARATOR . '..'. DIRECTORY_SEPARATOR . '..'. DIRECTORY_SEPARATOR. "Src" . DIRECTORY_SEPARATOR . 'Component');
-//         $controller = new WebComponentController($lib);
-//         $parsed_template = $controller->process(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR .  "WebComponents" . DIRECTORY_SEPARATOR ."RadioInputGroup.twig"));
-//         $this->assertEquals($this->integrationHtmlResult,$parsed_template);
-//     }
 
+    public function testAddAtribute()
+    {
+      $a =  '<a role="button" class=" btn-outlined"></a>';
+      $expect_data = '<!DOCTYPE html>' . "\n" . $a . "\n";
+      $button = '<!DOCTYPE html><wc-button className="btn-outlined"></wc-button>';
+      $result = $this->controller->process($button);
+      $this->assertEquals($expect_data, $result);
+    }
 
-  public function testAddAtribute()
-  {
-    $button = '<Button className="btn-outlined"></Button>';
-    $result = $this->controller->process($button);
-    $this->assertEquals('<a role="button" class=" btn-outlined"></a>', $result);
-  }
-
-  public function testLink() : void
-  {
-    $link = "<link rel=\"preload\" href=\"/static/build/css/yantramanav.min.css?v=1556611503\" as=\"style\" onload=\"this.onload=null;this.rel=\" 'stylesheet'\">";
-
-    $result = $this->controller->process($link);
-    $this->assertEquals($link, $result);
-  }
+    public function testLink() : void
+    {
+      $link = "<link rel=\"preload\" href=\"/static/build/css/yantramanav.min.css?v=1556611503\" as=\"style\" onload=\"this.onload=null;this.rel='stylesheet'\">";
+      $expect_data =     $expect_data = '<!DOCTYPE html>' . "\n" . $link . "\n";
+      $result = $this->controller->process("<!DOCTYPE html>".$link);
+      $this->assertEquals($expect_data, $result);
+    }
 }
